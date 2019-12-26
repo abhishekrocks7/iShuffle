888393b12b443d32fa806b37d420a3ffe119ab14
Abhishek <abhishekkambli3@gmail.com>
2019-12-26 08:52:05 +0530

    working changes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import {
   TogglePlayIcon,
@@ -8,8 +8,9 @@ import {
   OpenLinkIcon,
   MinimizeIcon,
   ExpandIcon,
+  CloseIcon,
 } from './Icons';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const PlayerView = styled(motion.div)`
   width: 160px;
@@ -44,7 +45,6 @@ const TogglePlayButton = styled(motion.div)`
   width: 56px;
   height: 56px;
   background: rgba(255, 255, 255, 0.2);
-  border: 1px solid rgba(255, 255, 255, 0.1);
   border-radius: 50%;
   cursor: pointer;
   display: grid;
@@ -110,8 +110,79 @@ const MimimizeView = styled(motion.div)`
   place-items: center;
 `;
 
+const Settings = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 2;
+  background-color: rgba(0, 0, 0, 0.7);
+  backdrop-filter: blur(16px);
+  border-radius: 8px;
+  display: grid;
+  place-items: center;
+  padding: 12px;
+`;
+
+const CloseView = styled(motion.div)`
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  right: 12px;
+  top: 12px;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.2);
+  display: grid;
+  cursor: pointer;
+  place-items: center;
+`;
+
+const Setting = styled.div`
+  width: 100%;
+`;
+
+const Label = styled.div`
+  font-size: 11px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: 500;
+  color: rgba(255, 255, 255, 0.7);
+  margin-bottom: 8px;
+`;
+
+const Toggle = styled.div`
+  width: 32px;
+  height: 20px;
+  border-radius: 20px;
+  transition: all 200ms ease-in-out;
+  background-color: ${props => (props.active ? '#fff' : 'rgba(255, 255, 255, 0.16)')};
+  position: relative;
+  cursor: pointer;
+`;
+
+const Knob = styled.div`
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background-color: ${props => (props.active ? '#000' : '#fff')};
+  position: absolute;
+  transition: all 200ms ease-in-out;
+  left: ${props => (props.active ? '14px' : '2px')};
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
+const Seperator = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: rgba(255, 255, 255, 0.1);
+`;
+
 function Player() {
   const [minimize, setMinimize] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [canMinimize, setCanMinimize] = useState(false);
 
   const handleTogglePlay = () => {
     console.log('toggle playPause click');
@@ -130,21 +201,84 @@ function Player() {
     chrome.runtime.sendMessage({ message: 'openLink' });
   };
 
+  const handleMinimize = () => {
+    setMinimize(!minimize);
+  };
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleMinimizeToggle = () => {
+    if (canMinimize) {
+      localStorage.setItem('canMinimize', 'no');
+      setCanMinimize(false);
+    } else {
+      localStorage.setItem('canMinimize', 'yes');
+      setCanMinimize(true);
+    }
+  };
+
+  useEffect(() => {
+    let value = localStorage.getItem('canMinimize');
+    if (value === 'yes') {
+      setCanMinimize(true);
+    } else {
+      setCanMinimize(false);
+    }
+  }, []);
+
   return (
     <PlayerView
       animate={{ width: minimize ? 32 : 160, height: minimize ? 32 : 160 }}
       drag="x"
       dragConstraints={{ left: 16, right: document.body.clientWidth - 176 }}
     >
-      <MimimizeView
-        isMinimized={minimize}
-        animate={
-          minimize ? { backgroundColor: 'rgba(0,0,0,0.0)' } : { backgroundColor: 'rgba(0,0,0,0.4)' }
-        }
-        onClick={() => setMinimize(!minimize)}
-      >
-        {minimize ? <ExpandIcon /> : <MinimizeIcon />}
-      </MimimizeView>
+      <AnimatePresence>
+        {showMenu && (
+          <Settings
+            key={'menu'}
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.98 }}
+          >
+            <CloseView
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.96, color: 'rgba(255, 255, 255, 0.32)' }}
+              onClick={() => handleMenu()}
+            >
+              <CloseIcon />
+            </CloseView>
+
+            <Setting>
+              <Label>Can Minimize</Label>
+              <Toggle active={canMinimize} onClick={() => handleMinimizeToggle()}>
+                <Knob active={canMinimize} />
+              </Toggle>
+            </Setting>
+            <Seperator />
+            <Setting>
+              <Label>Designed By</Label>
+              <Attribute href="https://www.twitter.com/_abhiii" target="_blank">
+                Abhi
+              </Attribute>
+            </Setting>
+          </Settings>
+        )}
+      </AnimatePresence>
+      {canMinimize && (
+        <MimimizeView
+          isMinimized={minimize}
+          animate={
+            minimize
+              ? { backgroundColor: 'rgba(0,0,0,0.0)' }
+              : { backgroundColor: 'rgba(0,0,0,0.4)' }
+          }
+          onClick={() => handleMinimize()}
+        >
+          {minimize ? <ExpandIcon /> : <MinimizeIcon />}
+        </MimimizeView>
+      )}
       <PlayerControlWheel
         animate={minimize ? { display: 'none', opacity: 0 } : { display: 'grid', opacity: 1 }}
       >
@@ -163,7 +297,9 @@ function Player() {
         >
           <PreviousTrackIcon />
         </PreviousTrackButton>
-        <MenuButton whileTap={{ scale: 0.92, opacity: 1 }}>Menu</MenuButton>
+        <MenuButton onClick={() => handleMenu()} whileTap={{ scale: 0.92, opacity: 1 }}>
+          Menu
+        </MenuButton>
         <OpenLinkButton onClick={() => handleOpenLink()} whileTap={{ scale: 0.92, opacity: 1 }}>
           <OpenLinkIcon />
         </OpenLinkButton>
