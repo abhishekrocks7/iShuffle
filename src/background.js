ca2b7068fb78b0ca2e7074a179d9a45048aa91db
Abhishek <abhishekkambli3@gmail.com>
2019-12-28 00:51:54 +0530

    same functionality as of last commit with content-background script architecture

--- src/background.js
+++ src/background.js
@@ -1,3 +1,7 @@
+function dispatchAction(tabId, code) {
+  chrome.tabs.executeScript(tabId, { code: code });
+}
+
 function getChannelUrl(channel) {
   switch (channel) {
     case 'Amazon':
@@ -8,22 +12,95 @@ function getChannelUrl(channel) {
       return 'https://soundcloud.com/*';
     case 'Youtube':
       return 'https://www.youtube.com/watch?*';
-
     default:
       return 'https://open.spotify.com/*';
   }
 }
 
-chrome.extension.onRequest.addListener(function(request, sender) {
-  if (
-    request.message === 'playNextTrack' ||
-    request.message === 'playPreviousTrack' ||
-    request.message === 'shuffleTracks' ||
-    request.message === 'togglePlayPause'
-  ) {
-    chrome.tabs.query({ url: getChannelUrl(request.channel) }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { message: request.message, tab: tabs[0].url });
-      console.log('request.message ', request.message, ' send to ', tabs[0].url);
-    });
+function runCodeFor(message, channel) {
+  if (channel === 'Spotify') {
+    switch (message) {
+      case 'playNextTrack':
+        return "document.querySelector('.spoticon-skip-forward-16').click()";
+      case 'playPreviousTrack':
+        return 'document.querySelector(".spoticon-skip-back-16").click()';
+      case 'togglePlayPause':
+        return `
+                    var pauseBttn = document.querySelector(".spoticon-pause-16");
+                    if(pauseBttn != null) {
+                        pauseBttn.click();
+                    } else {
+                        document.querySelector(".spoticon-play-16").click();
+                    }
+                `;
+      case 'shuffleTracks':
+        return 'document.querySelector(".spoticon-shuffle-16").click()';
+      default:
+        return '';
+    }
+  } else if (channel === 'Youtube') {
+    switch (message) {
+      case 'playNextTrack':
+        return 'document.querySelector(".ytp-next-button").click()';
+      case 'playPreviousTrack':
+        return 'window.history.back()';
+      case 'togglePlayPause':
+        return 'document.querySelector(".ytp-play-button").click()';
+      default:
+        return '';
+    }
+  } else if (channel === 'Amazon') {
+    switch (message) {
+      case 'playNextTrack':
+        return 'document.querySelector(\'[aria-label="Play next song"]\').click()';
+      case 'playPreviousTrack':
+        return 'document.querySelector(\'[aria-label="Previous song"]\').click()';
+      case 'togglePlayPause':
+        return 'document.querySelector(\'[aria-label="Play and pause"]\').click()';
+      case 'shuffleTracks':
+        return 'document.querySelector(".shuffleButton").click()';
+      default:
+        return '';
+    }
+  } else if (channel === 'SoundCloud') {
+    switch (message) {
+      case 'playNextTrack':
+        return 'document.querySelector(".skipControl.playControls__next.skipControl__next").click()';
+      case 'playPreviousTrack':
+        return 'document.querySelector(".playControls__prev.skipControl__previous").click()';
+      case 'togglePlayPause':
+        return 'document.querySelector(".playControls__control.playControls__play").click()';
+      case 'shuffleTracks':
+        return 'document.querySelector(".shuffleControl.sc-ir").click()';
+      default:
+        return '';
+    }
+  } else {
+    switch (message) {
+      case 'playNextTrack':
+        return 'document.querySelector(\'[aria-label="Next"]\').click()';
+      case 'playPreviousTrack':
+        return 'document.querySelector(\'[aria-label="Previous"]\').click()';
+      case 'togglePlayPause':
+        return `var appleMusicpauseBtn = document.querySelector(\'[aria-label="Pause"]\');
+                    if(appleMusicpauseBtn != null) {
+                        appleMusicpauseBtn.click();
+                    } else {
+                        document.querySelector(\'[aria-label="Play"]\').click();
+                    }`;
+      case 'shuffleTracks':
+        return 'document.querySelector(\'[aria-label="Shuffle"]\').click()';
+      default:
+        return '';
+    }
   }
+}
+
+chrome.extension.onRequest.addListener(function(request, sender) {
+  chrome.tabs.query({ url: getChannelUrl(request.channel) }, function(tabs) {
+    dispatchAction(
+      tabs && tabs.length >= 1 && tabs[0].id,
+      runCodeFor(request.message, request.channel)
+    );
+  });
 });
--- src/spotifyContentScript.js
+++ src/spotifyContentScript.js
@@ -1,28 +1,28 @@
-function spotifyControls() {
-  chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.message === 'playNextTrack') {
-      console.log('tabId is ', request.tabId);
-      document.querySelector('.spoticon-skip-forward-16').click();
-    }
-    if (request.message === 'playPreviousTrack') {
-      console.log('tabId is ', request.tabId);
-      document.querySelector('.spoticon-skip-back-16').click();
-    }
-    if (request.message === 'shuffleTracks') {
-      console.log('tabId is ', request.tabId);
-      let shuffleButton = document.querySelector('.spoticon-shuffle-16');
-      shuffleButton.click();
-    }
-    if (request.message === 'togglePlayPause') {
-      console.log('tabId is ', request.tabId);
-      var pauseBttn = document.querySelector('.spoticon-pause-16');
-      if (pauseBttn != null) {
-        pauseBttn.click();
-      } else {
-        document.querySelector('.spoticon-play-16').click();
-      }
-    }
-  });
-}
+// function spotifyControls() {
+//   chrome.runtime.onMessage.addListener(function(request, sender) {
+//     if (request.message === 'playNextTrack') {
+//       console.log('tabId is ', request.tabId);
+//       document.querySelector('.spoticon-skip-forward-16').click();
+//     }
+//     if (request.message === 'playPreviousTrack') {
+//       console.log('tabId is ', request.tabId);
+//       document.querySelector('.spoticon-skip-back-16').click();
+//     }
+//     if (request.message === 'shuffleTracks') {
+//       console.log('tabId is ', request.tabId);
+//       let shuffleButton = document.querySelector('.spoticon-shuffle-16');
+//       shuffleButton.click();
+//     }
+//     if (request.message === 'togglePlayPause') {
+//       console.log('tabId is ', request.tabId);
+//       var pauseBttn = document.querySelector('.spoticon-pause-16');
+//       if (pauseBttn != null) {
+//         pauseBttn.click();
+//       } else {
+//         document.querySelector('.spoticon-play-16').click();
+//       }
+//     }
+//   });
+// }
 
-spotifyControls();
+// spotifyControls();
