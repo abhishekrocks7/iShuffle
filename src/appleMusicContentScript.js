7ea228620b0a5fc25f6d911bac39da27256d8b12
Abhishek <abhishekkambli3@gmail.com>
2019-12-29 21:23:27 +0530

    added apple music support for shuffle state

--- src/appleMusicContentScript.js
+++ src/appleMusicContentScript.js
@@ -3,19 +3,24 @@ function appleMusicControls() {
     if (request.message === 'playNextTrack') {
       console.log('tabId is ', request.tabId);
       document.querySelector('[aria-label="Next"]').click();
+      enableEventListener();
     }
     if (request.message === 'playPreviousTrack') {
       console.log('tabId is ', request.tabId);
       document.querySelector('[aria-label="Previous"]').click();
+      enableEventListener();
     }
     if (request.message === 'shuffleTracks') {
       console.log('tabId is ', request.tabId);
       let shuffleButton = document.querySelector('[aria-label="Shuffle"]');
       shuffleButton.click();
+
+      enableEventListener();
     }
     if (request.message === 'togglePlayPause') {
       console.log('tabId is ', request.tabId);
       var appleMusicpauseBtn = document.querySelector('[aria-label="Pause"]');
+      enableEventListener();
       if (appleMusicpauseBtn != null) {
         appleMusicpauseBtn.click();
       } else {
@@ -26,3 +31,25 @@ function appleMusicControls() {
 }
 
 appleMusicControls();
+
+function findShuffleState() {
+  setTimeout(() => {
+    if (document.querySelector('[aria-label="Shuffle"]').classList.contains('active')) {
+      console.log('shuffle is active');
+      chrome.runtime.sendMessage({ type: 'shuffleState', message: 'active', channel: 'Apple' });
+    } else {
+      console.log('shuffle is inactive');
+      chrome.runtime.sendMessage({
+        type: 'shuffleState',
+        message: 'inactive',
+        channel: 'Apple',
+      });
+    }
+  }, 1000);
+}
+
+function enableEventListener() {
+  document.querySelector('[aria-label="Shuffle"]').addEventListener('click', () => {
+    findShuffleState();
+  });
+}
