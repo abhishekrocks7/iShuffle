f52a4a884c955a9ffced2414a218d7080aab84a4
Abhishek <abhishekkambli3@gmail.com>
2020-01-05 00:29:21 +0530

    added appleMusic contentScript

--- src/appleMusicContentScript.js
+++ src/appleMusicContentScript.js
@@ -53,3 +53,31 @@
 //     findShuffleState();
 //   });
 // }
+
+function sendMessage(type, message) {
+  chrome.runtime.sendMessage({
+    service: 'Apple',
+    type,
+    message,
+    origin: 'iShuffle',
+  });
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.origin === 'iShuffle') {
+    switch (request.message) {
+      case 'shuffleTracks':
+        setTimeout(() => {
+          sendMessage(
+            'shuffleState',
+            document.querySelector('[aria-label="Shuffle"]').classList.contains('active')
+              ? 'active'
+              : 'inactive'
+          );
+        }, 1000);
+        break;
+      default:
+        break;
+    }
+  }
+});
