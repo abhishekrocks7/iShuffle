90bf83336ec79a1acdc5d376017c1a6f25ff2602
Abhishek <abhishekkambli3@gmail.com>
2020-01-08 00:09:24 +0530

    back to previous design

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,16 +2,14 @@ import React, { useState, useEffect, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import styled from 'styled-components';
 import {
-  SmallSpotifyIcon,
+  SpotifyIcon,
   DropdownIcon,
   TogglePlayIcon,
-  PlayIcon,
   NextTrackIcon,
   PreviousTrackIcon,
   ShuffleTrackIcon,
-  MoreIcon,
-  PauseIcon,
   YouTubeIcon,
+  CloseIcon,
   AppleIcon,
   AmazonIcon,
   SoundCloudIcon,
@@ -32,79 +30,126 @@ const PlayerView = styled(motion.div)`
   box-sizing: border-box;
   position: fixed;
   bottom: 12px;
-
   display: grid;
-  grid-template-columns: 48px 1px 32px 32px 32px 1px 32px;
-  grid-column-gap: 12px;
-  height: 56px;
-  padding: 12px;
+  width: 144px;
+  height: 144px;
   background: linear-gradient(
-      95.87deg,
-      #080811 0.29%,
-      #0b2020 20.46%,
-      #0f0e36 49.95%,
-      #330b23 99.61%
+      105.29deg,
+      #080811 -17.67%,
+      #0b2020 8.19%,
+      #0f0e36 45.98%,
+      #330b23 109.63%
     ),
     #080811;
   border-radius: 12px;
   z-index: 999999999999999;
+  place-items: center;
 `;
 
-const Seperator = styled.div`
-  width: 1px;
-  height: 32px;
-  background-color: rgba(255, 255, 255, 0.1);
+const WheelView = styled.div`
+  width: 90%;
+  height: 90%;
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.06);
+  position: relative;
+  display: grid;
+  grid-template-rows: 36px auto 36px;
+  grid-template-columns: 36px auto 36px;
 `;
 
 const ActionItem = styled(motion.div)`
   width: 100%;
-  height: 32px;
+  height: 100%;
   display: grid;
   place-items: center;
+  align-items: center;
   cursor: pointer;
-  background-color: ${props =>
-    props.type === 'serviceSwitcher' || props.active
-      ? 'rgba(55, 225, 163, 0.1)'
-      : 'rgba(255, 255, 255, 0.1)'};
-  border-radius: 16px;
+  box-shadow: ${props => props.type === 'playPause' && '0 0 0px 1px rgba(0,0,0,0.6)'};
+  background-color: ${props => props.type === 'playPause' && 'rgba(0,0,0,0.6)'};
+  border-radius: ${props => props.type === 'playPause' && '50%'};
   svg path {
     fill: ${props => (props.active ? '#37E1A3' : '#fff')};
   }
+  grid-area: ${props =>
+    props.type === 'playPause'
+      ? '2 / 2'
+      : props.type === 'next'
+      ? '2/3/3/3'
+      : props.type === 'previous'
+      ? '2/2/2/1'
+      : props.type === 'shuffle'
+      ? '2/2/1/2'
+      : '3/2'};
 `;
 
 const SelectedServiceWrapper = styled.div`
   display: grid;
   grid-template-columns: 20px 5px;
-  grid-gap: 4px;
+  grid-gap: 3px;
   align-items: center;
-
   svg path {
     fill: #37e1a3;
   }
-
   svg:last-child {
     transform: ${props => (props.isServiceSelectionOpen ? 'rotate(180deg)' : 'roate(0deg)')};
   }
 `;
 
-const ServiceSelector = styled(motion.div)`
-  background: rgba(0, 0, 0, 0.4);
-  height: 56px;
+const ServiceSwitcherView = styled(motion.div)`
+  width: 100%;
+  height: 100%;
   position: absolute;
+  top: 0;
   left: 0;
   right: 0;
   bottom: 0;
+  background-color: rgba(0, 0, 0, 0.9);
+  backdrop-filter: blur(20px);
+  z-index: 2;
+  border-radius: 8px;
+  overflow: hidden;
+`;
+
+const List = styled(motion.div)`
   display: grid;
-  grid-template-columns: repeat(5, 40px);
-  grid-gap: 12px;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  place-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const ServiceWrapper = styled(motion.div)`
+  cursor: pointer;
+  opacity: 0.7;
+  width: 100%;
+  height: 100%;
+  display: flex;
   align-items: center;
   justify-content: center;
-  border-bottom-left-radius: 12px;
-  border-bottom-right-radius: 12px;
+  transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
+  overflow: hidden;
+  svg {
+    transform: scale(1.4);
+  }
+
+  &:hover {
+    background-color: ${props =>
+      props.service === 'Spotify'
+        ? '#1FBA5D'
+        : props.service === 'Apple'
+        ? '#0057ff'
+        : props.service === 'YouTube'
+        ? 'rgba(254, 53, 89, 1)'
+        : props.service === 'Amazon'
+        ? '#19A0B2'
+        : 'rgba(254, 121, 25, 1)'};
+  }
 `;
 
 function Player() {
   //States
+  const [services] = useState(['Spotify', 'Apple', 'YouTube', 'Amazon', 'SoundCloud']);
   const [activeService, setActiveService] = useState('Spotify');
   const [isShuffleActive, setShuffleActive] = useState(false);
   const [isPlaying, setPlaying] = useState(false);
@@ -140,7 +185,7 @@ function Player() {
   const renderIconForService = service => {
     switch (service) {
       case 'Spotify':
-        return <SmallSpotifyIcon />;
+        return <SpotifyIcon />;
       case 'Apple':
         return <AppleIcon />;
       case 'YouTube':
@@ -186,93 +231,75 @@ function Player() {
     <Fragment>
       <Wrapper isServiceSelectionOpen={isServiceSelectionOpen} ref={constraintsRef} />
       <PlayerView
+        animate={{
+          visibility: 'visible',
+          height: isServiceSelectionOpen ? 240 : 144,
+        }}
         drag
         dragConstraints={constraintsRef}
-        animate={{
-          height: isServiceSelectionOpen ? 112 : 56,
-          borderRadius: isServiceSelectionOpen ? 12 : 32,
-        }}
       >
-        <ActionItem type="serviceSwitcher" onClick={() => openServiceSelectionMenu()}>
-          <SelectedServiceWrapper isServiceSelectionOpen={isServiceSelectionOpen}>
-            {renderIconForService(activeService)}
-            <DropdownIcon />
-          </SelectedServiceWrapper>
-        </ActionItem>
-        <Seperator />
-        <ActionItem
-          onClick={() => handleAction(activeService, 'playPreviousTrack')}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.94 }}
-        >
-          <PreviousTrackIcon />
-        </ActionItem>
-        <ActionItem
-          onClick={() => handleAction(activeService, 'togglePlayPause')}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.94 }}
-        >
-          <TogglePlayIcon />
-        </ActionItem>
-        <ActionItem
-          onClick={() => handleAction(activeService, 'playNextTrack')}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.94 }}
-        >
-          <NextTrackIcon />
-        </ActionItem>
-        <Seperator />
-        <ActionItem
-          onClick={() => handleAction(activeService, 'shuffleTracks')}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.94 }}
-          active={isShuffleActive}
-        >
-          <ShuffleTrackIcon />
-        </ActionItem>
+        <WheelView>
+          <ActionItem type="serviceSwitcher" onClick={() => openServiceSelectionMenu()}>
+            <SelectedServiceWrapper isServiceSelectionOpen={isServiceSelectionOpen}>
+              {renderIconForService(activeService)}
+              <DropdownIcon />
+            </SelectedServiceWrapper>
+          </ActionItem>
+
+          <ActionItem
+            type="previous"
+            onClick={() => handleAction(activeService, 'playPreviousTrack')}
+            whileTap={{ scale: 0.94 }}
+          >
+            <PreviousTrackIcon />
+          </ActionItem>
+          <ActionItem
+            type="playPause"
+            onClick={() => handleAction(activeService, 'togglePlayPause')}
+            whileTap={{ scale: 0.94 }}
+          >
+            <TogglePlayIcon />
+          </ActionItem>
+          <ActionItem
+            type="next"
+            onClick={() => handleAction(activeService, 'playNextTrack')}
+            whileTap={{ scale: 0.94 }}
+          >
+            <NextTrackIcon />
+          </ActionItem>
+
+          <ActionItem
+            type="shuffle"
+            onClick={() => handleAction(activeService, 'shuffleTracks')}
+            whileTap={{ scale: 0.94 }}
+            active={isShuffleActive}
+          >
+            <ShuffleTrackIcon />
+          </ActionItem>
+        </WheelView>
         <AnimatePresence>
           {isServiceSelectionOpen && (
-            <ServiceSelector
-              initial={{ height: 0, visibility: 'hidden', opacity: 0 }}
-              animate={{ height: 56, visibility: 'visible', opacity: 1 }}
-              exit={{ height: 0, visibility: 'hidden', opacity: 0 }}
+            <ServiceSwitcherView
+              key="channelSwitcher"
+              initial={{ scale: 0, display: 'none' }}
+              animate={{ scale: 1, display: 'block' }}
+              exit={{ opacity: 0, display: 'none' }}
             >
-              <ActionItem
-                onClick={() => handleServiceSelection('Spotify')}
-                whileHover={{ backgroundColor: '#1FBA5D' }}
-                whileTap={{ scale: 0.92 }}
-              >
-                <SmallSpotifyIcon />
-              </ActionItem>
-              <ActionItem
-                onClick={() => handleServiceSelection('Apple')}
-                whileHover={{ backgroundColor: 'rgba(0, 122, 255, 1)' }}
-                whileTap={{ scale: 0.92 }}
-              >
-                <AppleIcon />
-              </ActionItem>
-              <ActionItem
-                onClick={() => handleServiceSelection('YouTube')}
-                whileHover={{ backgroundColor: 'rgba(254, 53, 89, 1)' }}
-                whileTap={{ scale: 0.92 }}
-              >
-                <YouTubeIcon />
-              </ActionItem>
-              <ActionItem
-                onClick={() => handleServiceSelection('Amazon')}
-                whileHover={{ backgroundColor: '#19A0B2' }}
-                whileTap={{ scale: 0.92 }}
-              >
-                <AmazonIcon />
-              </ActionItem>
-              <ActionItem
-                onClick={() => handleServiceSelection('SoundCloud')}
-                whileHover={{ backgroundColor: 'rgba(254, 121, 25, 1)' }}
-                whileTap={{ scale: 0.92 }}
-              >
-                <SoundCloudIcon />
-              </ActionItem>
-            </ServiceSelector>
+              <List>
+                {services &&
+                  services.map(service => (
+                    <ServiceWrapper
+                      whileTap={{ scale: 0.96 }}
+                      whileHover={{ opacity: 1 }}
+                      service={service}
+                      onClick={() => handleServiceSelection(service)}
+                      key={service}
+                    >
+                      {renderIconForService(service)}
+                    </ServiceWrapper>
+                  ))}
+              </List>
+            </ServiceSwitcherView>
           )}
         </AnimatePresence>
       </PlayerView>
